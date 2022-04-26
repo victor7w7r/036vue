0223dd537de6f9d6c7874f194f5806a9a70593c9
Victor Jiménez <vcjimenez1@espe.edu.ec>
2022-04-26 11:13:36 -0500

    Update Packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
 import { fileURLToPath } from "url";
+import { resolve, dirname } from 'path';
 
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
-import path from 'path';
 
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "src"),
+      "@": resolve(dirname(fileURLToPath(import.meta.url)), "src"),
     },
   }
 });
