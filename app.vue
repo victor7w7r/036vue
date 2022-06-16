7347667f5c52e5b269ed6f7b9cc11b98946e53e1
Victor Jiménez <vcjimenez1@espe.edu.ec>
2022-06-16 16:10:54 -0500

    AddNuxtBlank

--- app.vue
+++ app.vue
@@ -2,23 +2,7 @@
     <Head>
         <Title>036nuxt</Title>
     </Head>
-    <Body class="antialiased dark">
-        <div :class="`h-screen transition-colors duration-1000 ${whiteBackground} ${darkBackground}`" >
-            <NuxtLayout><NuxtPage/></NuxtLayout>
-            <Header />
-        </div>
-    </Body>
+    <div >
+        <NuxtLayout><NuxtPage/></NuxtLayout>
+    </div>
 </template>
-
-<script setup lang="ts">
-import { useTheme } from '~/stores';
-const themeStore = useTheme();
-
-onMounted(() => {
-    const isDark: boolean = document.body.classList.contains("dark");
-    isDark ? themeStore.setDarkMode(true) : themeStore.setDarkMode(false); 
-});
-
-const whiteBackground = computed(() => themeStore.getWhiteBackground);
-const darkBackground = computed(() => themeStore.getDarkBackground);
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,5 @@
 import { defineNuxtConfig } from 'nuxt';
 
 export default defineNuxtConfig({
-
-    buildModules: ['@pinia/nuxt', '@nuxtjs/tailwindcss'],
-    modules: ['@vueuse/nuxt'],
-    build: {
-        postcss: {
-            postcssOptions: {
-                plugins: { tailwindcss: {}, autoprefixer: {}}
-            }
-        }
-    },
-    tailwindcss: {
-        cssPath: '~/assets/scss/tailwind.scss',
-        configPath: 'tailwind.config.js',
-        exposeConfig: false,
-        config: {},
-        injectPosition: 0,
-        viewer: false
-    }
+    buildModules: ['@pinia/nuxt']
 });
